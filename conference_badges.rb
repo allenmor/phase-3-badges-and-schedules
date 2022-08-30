a44782a5b85558dd058a55fbef5034f828b96346
Allen Mordukhaev <allenmor93@gmail.com>
2022-08-29 20:25:44 -0400

    commit tree 9743a7778f56da03ee7912e5bd2c4cca6427d0ff

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def badge_maker(name1, name2)
+    new_array = ["Hello, my name is #{name1}.", "Hello, my name is #{name2}."]
+
+end
